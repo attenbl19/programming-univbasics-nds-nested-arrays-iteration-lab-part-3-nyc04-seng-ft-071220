02a155df4022b082efe49a2816a0650514a53990
Lida Attenborough <attenboroughl@protonmail.com>
2020-07-20 15:54:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,8 @@ def join_nested_strings(src)
   while rox_index < src.count do
     element_index = 0
     while element_index < src[rox_index].count do
-      if src[rox_index][element_index].class == String src[rox_index][element_index]
+      if src[rox_index][element_index].class == String
+        new_string src[rox_index][element_index]
       element_index += 1
     end
     rox_index += 1
